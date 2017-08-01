c1ece47568051ea9cc0447a31173b64ce494d24e
Tyler Silva <me@tylerbsilva.com>
2017-07-31 23:02:18 -0700

    Start to add elements for full SPA experience

--- public/main.js
+++ public/main.js
@@ -1,10 +1,17 @@
 
 
-function main(){
-  alert('alskjdf');
+function main(id){
+  var entry = document.getElementById(id);
+  
+  var header = document.createElement('h1');
+  var headerText = document.createTextNode("Workout Tracker");
+
+  header.appendChild(headerText);
+  console.log(header);
+  entry.appendChild(header);
 };
 
-(function(){ 
-  window.addEventListener("DOMContentLoaded", main);
-})();
+(function(id){ 
+  document.addEventListener("load", main(id));
+})('app');
 
